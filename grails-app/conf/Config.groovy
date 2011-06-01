a20a8e66c2ad41fd3042ab1d719b00ad255bdca7
Tony Strauss <tony@blackberry.local>
2011-06-01 13:40:05 -0400

    .

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -93,13 +93,3 @@ images = [
   'http://s3-p.animoto.com/Image/blFNy7LuTOCCRbLZIaYvBg/l.jpg',
   'http://s3-p.animoto.com/Image/RSlVAvr57bj1de7YxSf9RQ/l.jpg'
 ]
-
-// These are our Animoto credentials for the Workflow Widget and Platform API.
-widget.partnerId      = "ac42be375feb79fe55e6"
-widget.partnerSecret  = "93823b7bed3552131a7f"
-widget.host           = "https://widget-service-sandbox.animoto.com"
-widget.appId          = "46053ed4739ffc1aa0c5"
-
-platform.key          = "99320bd08867012dc34a12313b045877"
-platform.secret       = "867319c36489597437c6965fea6b1512d7ece403"
-api2.host             = "https://platform-sandbox.animoto.com"
